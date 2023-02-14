0fdbed5694c28d5103658572318080a2e2200af1
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-02-15 02:19:03 +0300

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+  int n = (number / 10) % 10;
+  Console.WriteLine($"Вторая цифра числа: {n}");
+}
+else
+  Console.WriteLine("Число не трехзначиное");
