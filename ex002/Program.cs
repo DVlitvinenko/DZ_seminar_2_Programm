2770df7416ee5f4aaa9bab31dcf7fca474f062e2
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-02-15 02:30:35 +0300

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99)
+{
+  string n = Convert.ToString(number);
+  Console.WriteLine($"Третья цифра числа: {n[2]}");
+}
+else
+  Console.WriteLine("Третей цифры нет");
